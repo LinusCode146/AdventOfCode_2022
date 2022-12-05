d0ece6cb39d90a0a337ae3a421313482b8219837
Viper <117307956+LinusCode146@users.noreply.github.com>
2022-12-05 17:41:06 +0100

    Add files via upload

---
+++ Day5/part2.py
@@ -0,0 +1,36 @@
+from pprint import pprint
+
+
+stacks = {
+    '1': ['J', 'F', 'C', 'N', 'D', 'B', 'W'],
+    '2': ['T', 'S', 'L', 'Q', 'V', 'Z', 'P'],
+    '3': ['T', 'J', 'G', 'B', 'Z', 'P'],
+    '4': ['C', 'H', 'B', 'Z', 'J', 'L', 'T', 'D'],
+    '5': ['S', 'J', 'B', 'V', 'G'],
+    '6': ['Q', 'S', 'P'],
+    '7': ['N', 'P', 'M', 'L', 'F', 'D', 'V', 'B'],
+    '8': ['R', 'L', 'D', 'B', 'F', 'M', 'S', 'P'],
+    '9': ['R', 'T', 'D', 'V']
+}
+
+lines = [line.rstrip().split() for line in open('info.in')]
+
+
+for item in lines:
+    f = item[3]
+    t = item[5]
+    containers = []
+    for _ in range(int(item[1])):
+        element = stacks[f].pop(0)
+        containers.append(element)
+    for elem in containers[::-1]:
+        stacks[t].insert(0, elem)
+
+solution = ''
+for value in stacks.values():
+    solution += value[0]
+print(solution)
+    
+
+    
+    
